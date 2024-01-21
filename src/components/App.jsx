cae0dfc4a441e63eaa91b720310da0c21dc4a2a8
mir-olga <olga.mirzoeva21@gmail.com>
2024-01-21 18:02:55 +0200

    Part 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,40 +1,24 @@
-/*import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import userData from '../userData.json';
+import { Profile } from './Profile/Profile';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
     </>
-  )
-}
+  );
+};
 
-export default App*/
 
-import { Product } from './Product';
+
+
+/*import { Product } from './Product';
 
 /*export default function App() {
   return (
@@ -48,21 +32,21 @@ import { Product } from './Product';
   );
 }*/
 
-export default function App() {
+/*export default function App() {
   return (
     <div>
       <h1>Best selling</h1>
 
       <Product
         name="Tacos With Lime"
-        imgUrl="<https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640>"
+        imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
         price={10.99}
       />
       <Product
         name="Fries and Burger"
-        imgUrl="<https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640>"
+        imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
         price={14.29}
       />
     </div>
   );
-}
+}*/
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,15 +1,3 @@
-/*export const Product = () => {
-	const price = 999;
-	const imgUrl = "<https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640>";
-
-  return (
-    <>
-      <h2>Tacos</h2>
-			<img src={imgUrl} alt="Tacos With Lime" width="640" />
-      <p>Price: {price} credits</p>
-    </>
-  );
-};*/
 
 export const Product = ({ name, imgUrl, price }) => {
   return (
